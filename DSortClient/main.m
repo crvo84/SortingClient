b7eba2d9bc7f82e9c8d9138f341687a95c299aaa
Carlos R. Villanueva Ousset <crvo84@gmail.com>
2017-09-21 02:54:13 -0500

    Command line arguments.

--- DSortClient/main.m
+++ DSortClient/main.m
@@ -14,9 +14,33 @@
 int main(int argc, const char * argv[])
 {
     @autoreleasepool {
-        int n = 1000;
-        int m = 4; // max 4
+        // 2 arguments
+        if (argc != 3) {
+            NSLog(@"Two arguments required.\nn = size of array\nm = number of servers (max 4)");
+            return 0;
+        }
+        
+        int n = atoi(argv[1]);
+        int m = atoi(argv[2]);
         int max = 999;
+        
+        // max 4 servers
+        if (m > 4) {
+            NSLog(@"Max 4 servers.");
+            return 0;
+        }
+        
+        // n multiplo de m
+        if (n % m != 0) {
+            NSLog(@"n % m != 0");
+            return 0;
+        }
+        
+        // n minimo 2m
+        if (n < 2*m) {
+            NSLog(@"n < 2*m");
+            return 0;
+        }
         
         NSArray *randomArray = [RandomNumbersGenerator arrayOfIntegerNumbersWithSize:n maximum:max];
         
@@ -68,6 +92,8 @@ int main(int argc, const char * argv[])
         NSDate *localEnd = [NSDate date];
         
         double localComputationTime = [localEnd timeIntervalSinceDate:localStart];
+        
+        
 
         NSLog(@"-----------------------------------\nn = %d\nm = %d\n-----------------------------------\nDISTRIBUIDO\nTiempo Total = %f\nTiempo de Comunicación = %f\nTiempo de Computación = %f\n-----------------------------------\nLOCAL\nTiempo de Computación = %f\n-----------------------------------", n, m, serverManager.getMaxComputationTime + serverManager.getMaxCommunicationTime, serverManager.getMaxCommunicationTime, serverManager.getMaxComputationTime, localComputationTime);
     }
