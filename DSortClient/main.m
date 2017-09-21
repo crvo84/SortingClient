22440acbfc7d0ac12ce8b36202c95935def348d0
Carlos R. Villanueva Ousset <crvo84@gmail.com>
2017-09-21 00:07:48 -0500

    Implement multiple simultaneous server calls for sorting.

--- DSortClient/main.m
+++ DSortClient/main.m
@@ -9,28 +9,46 @@
 #import <Foundation/Foundation.h>
 #import "MergeHelper.h"
 #import "DSortServerManager.h"
+#import "RandomNumbersGenerator.h"
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     @autoreleasepool {
-        BOOL distributed = true;
+        BOOL distributed = YES;
+        int n = 1000;
+        int m = 4; // max 4
+        int max = 999;
         
-        if (distributed) {
-            NSArray *array = @[@2, @1, @8, @6, @0, @1000, @3, @67, @999];
+        if (distributed) { // distributed merge
+            int subarraySize = n/m; // size of each array to be sorted by each server
+            
+            NSMutableArray *subarraysToSort = [[NSMutableArray alloc] init]; // array of arrays
+            for (int i = 0; i < m; i++) {
+                NSArray *subarray = [RandomNumbersGenerator arrayOfIntegerNumbersWithSize:subarraySize
+                                                                                  maximum:max];
+                [subarraysToSort insertObject:subarray atIndex:i];
+            }
             
             DSortServerManager *serverManager = [[DSortServerManager alloc] initWithNumberOfServers:1];
             
             dispatch_group_t serviceGroup = dispatch_group_create();
             
-            dispatch_group_enter(serviceGroup);
-            [serverManager askServerWithIndex:0 toSortArray:array withCompletion:^(NSError *error) {
-                if (error) {
-                    NSLog(@"%@", error);
-                }
-                
-                dispatch_group_leave(serviceGroup);
-            }];
-            
+            for (int i = 0; i < subarraysToSort.count; i++) {
+                dispatch_group_enter(serviceGroup);
+                [serverManager askServerWithIndex:i // server index
+                                      toSortArray:subarraysToSort[i]
+                                   withCompletion:^(NSError *error) {
+                    if (error) { NSLog(@"%@", error); }
+                    
+                    dispatch_group_leave(serviceGroup);
+                }];
+            }
+            // all m subarrays must be sorted at this point
             dispatch_group_wait(serviceGroup, DISPATCH_TIME_FOREVER);
+            
+            // TODO: merge sorted subarrays
+            NSLog(@"");
+            
         } else {
             // TODO: merge locally
         }
